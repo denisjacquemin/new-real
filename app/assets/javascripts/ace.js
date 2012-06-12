3b472a1b298697db9052c65867fa6dfe98364315
Denis Jacquemin <denis.jacquemin@gmail.com>
2012-06-12 08:52:44 +0200

    components pages 'PJaxified' by creating initAce attached to window

--- app/assets/javascripts/ace.js
+++
@@ -1,4 +0,0 @@
-//= require ace/src/ace
-//= require ace/src/theme-twilight
-//= require ace/src/mode-html
-//= require init-ace
--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -3,5 +3,9 @@
 //= require twitter/bootstrap
 //= require pjax
 //= require_tree ./admin
+//= require ace/src/ace
+//= require ace/src/theme-twilight
+//= require ace/src/mode-html
+//= require init-ace
 
 
